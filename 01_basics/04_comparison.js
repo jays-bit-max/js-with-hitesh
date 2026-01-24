aea83c23e39a15d04e75ce81bb1b31aff24c1648
Jays <jaysolage@gmail.com>
2026-01-24 16:55:44 +0000

    some comparisons checked

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+
+// console.log("2" > 1); // => true(Dont know the reason it is drawback)
+
+// console.log(null == 0); // => false
+
+// console.log((undefined == 0)); // => false
+
+console.log("02" == 2); // here == is doesnt check its datatypes just give comparison and op is => true
+console.log("02" === 2); // here === is used to strictly check its datatypes and values and so why op is => false , and thats correct
+
+
+
